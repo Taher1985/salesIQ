b7cf44d902c20732f1ff5278d7fe3ea3e82d6c9a
1206816 <1206816@01HW742075.tcsgegdc.com>
2017-01-04 13:25:18 +0530

    added

---
+++ src/main/java/com/ge/salesIQ/SalesIqApplication.java
@@ -0,0 +1,12 @@
+package com.ge.salesIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesIqApplication.class, args);
+	}
+}
